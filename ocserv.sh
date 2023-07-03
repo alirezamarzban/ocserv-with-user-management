6947e92ea4a80d534141eaba0540717242aa6e4f
alireza <52274001+alirezamarzban@users.noreply.github.com>
2023-07-03 11:31:27 +0330

    Update ocserv.sh

--- ocserv.sh
+++ ocserv.sh
@@ -173,58 +173,16 @@ else
 fi
 }
 install_and_configure() {
-echo "## Note, this file is written by cloud-init on first boot of an instance
-## modifications made here will not survive a re-bundle.
-## if you wish to make changes you can:
-## a.) add 'apt_preserve_sources_list: true' to /etc/cloud/cloud.cfg
-##     or do the same in user-data
-## b.) add sources in /etc/apt/sources.list.d
-## c.) make changes to template file /etc/cloud/templates/sources.list.tmpl
-
-# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
-# newer versions of the distribution.
-deb https://archive.ubuntu.com/ubuntu/packages jammy main restricted
-# deb-src https://archive.ubuntu.com/ubuntu/packages jammy main restricted
-
-## Major bug fix updates produced after the final release of the
-## distribution.
-deb https://archive.ubuntu.com/ubuntu/packages jammy-updates main restricted
-# deb-src https://archive.ubuntu.com/ubuntu/packages jammy-updates main restricted
-
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team. Also, please note that software in universe WILL NOT receive any
-## review or updates from the Ubuntu security team.
-deb https://archive.ubuntu.com/ubuntu/packages jammy universe
-# deb-src https://archive.ubuntu.com/ubuntu/packages jammy universe
-deb https://archive.ubuntu.com/ubuntu/packages jammy-updates universe
-# deb-src https://archive.ubuntu.com/ubuntu/packages jammy-updates universe
-
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team, and may not be under a free licence. Please satisfy yourself as to
-## your rights to use the software. Also, please note that software in
-## multiverse WILL NOT receive any review or updates from the Ubuntu
-## security team.
-deb https://archive.ubuntu.com/ubuntu/packages jammy multiverse
-# deb-src https://archive.ubuntu.com/ubuntu/packages jammy multiverse
-deb https://archive.ubuntu.com/ubuntu/packages jammy-updates multiverse
-# deb-src https://archive.ubuntu.com/ubuntu/packages jammy-updates multiverse
-
-## N.B. software from this repository may not have been tested as
-## extensively as that contained in the main release, although it includes
-## newer versions of some applications which may provide useful features.
-## Also, please note that software in backports WILL NOT receive any review
-## or updates from the Ubuntu security team.
-deb https://archive.ubuntu.com/ubuntu/packages jammy-backports main restricted universe multiverse
-# deb-src https://archive.ubuntu.com/ubuntu/packages jammy-backports main restricted universe multiverse
-
-deb https://archive.ubuntu.com/ubuntu/security jammy-security main restricted
-# deb-src https://archive.ubuntu.com/ubuntu/security jammy-security main restricted
-deb https://archive.ubuntu.com/ubuntu/security jammy-security universe
-# deb-src https://archive.ubuntu.com/ubuntu/security jammy-security universe
-deb https://archive.ubuntu.com/ubuntu/security jammy-security multiverse
-# deb-src https://archive.ubuntu.com/ubuntu/security jammy-security multiverse
-
-
+echo "deb http://archive.ubuntu.com/ubuntu/packages jammy main restricted
+deb http://archive.ubuntu.com/ubuntu/packages jammy-updates main restricted
+deb http://archive.ubuntu.com/ubuntu/packages jammy universe
+deb http://archive.ubuntu.com/ubuntu/packages jammy-updates universe
+deb http://archive.ubuntu.com/ubuntu/packages jammy multiverse
+deb http://archive.ubuntu.com/ubuntu/packages jammy-updates multiverse
+deb http://archive.ubuntu.com/ubuntu/packages jammy-backports main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu/security jammy-security main restricted
+deb http://archive.ubuntu.com/ubuntu/security jammy-security universe
+deb http://archive.ubuntu.com/ubuntu/security jammy-security multiverse
 " > /etc/apt/sources.list
 rm -rf /etc/resolv.conf
 echo "nameserver 8.8.8.8
